fa20eae299c2dc3d773fbf2f972e9ad2a76cb4a8
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-07-19 23:04:36 +0200

    get data from api

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import products from './data/products';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is running!');
