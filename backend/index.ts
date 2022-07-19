9af446d040cc9158133746047d98cfa6ddc6512b
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-07-19 21:55:05 +0200

    add '/api/products' endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import products from './data/products';
 
 dotenv.config();
 
@@ -8,6 +9,19 @@ const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is running!');
+});
+
+app.get('/api/products', (req: Request, res: Response) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req: Request, res: Response) => {
+  const product = products.find(p => p._id === req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Product not found');
+  }
 });
 
 app.listen(port, () => {
