fd2f8883a521c0884f3b013da32879653bed653b
Kacper <kpawlak2000@gmail.com>
2022-07-21 12:16:17 +0200

    add models

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = mongoose.connect(`${process.env.MONGO_URI}`);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
 
-    console.log(`MongoDB Connected`);
+    console.log(`MongoDB Connected ${conn.connection.host}`);
   } catch (error: any) {
-    console.error(error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
