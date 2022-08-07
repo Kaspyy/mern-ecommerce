05ba89f105fbfa0db5ed2ecc344c833b194ce149
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-08-07 19:01:24 +0200

    add OrderScreen

--- frontend/src/store/actions/orderActions.ts
+++ frontend/src/store/actions/orderActions.ts
@@ -4,6 +4,9 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_FAILURE,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAILURE,
 } from '../constants/orderConstants';
 import { AppDispatch } from '../store';
 
@@ -34,6 +37,41 @@ export const createOrder = (order: Order) => {
     } catch (error: any) {
       dispatch({
         type: ORDER_CREATE_FAILURE,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+};
+
+export const getOrderDetails = (orderId: string) => {
+  return async (dispatch: AppDispatch, getState: Function) => {
+    try {
+      dispatch({
+        type: ORDER_DETAILS_REQUEST,
+      });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.get(`/api/orders/${orderId}`, config);
+
+      dispatch({
+        type: ORDER_DETAILS_SUCCESS,
+        payload: data,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: ORDER_DETAILS_FAILURE,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
--- frontend/src/store/reducers/orderReducers.ts
+++ frontend/src/store/reducers/orderReducers.ts
@@ -2,6 +2,9 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_FAILURE,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_FAILURE,
+  ORDER_DETAILS_SUCCESS,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (
@@ -22,6 +25,31 @@ export const orderCreateReducer = (
     case ORDER_CREATE_FAILURE:
       return {
         ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const orderDetailsReducer = (
+  state = { loading: true, orderItems: [], shippingAddress: {} },
+  action: { type: any; payload: any }
+) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ORDER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        order: action.payload,
+      };
+    case ORDER_DETAILS_FAILURE:
+      return {
         loading: false,
         error: action.payload,
       };
